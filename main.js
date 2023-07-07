bd4c7a068a8cc92ceeec244a8fdf698c4025964c
Kirkster101 <83754901+Kirkster101@users.noreply.github.com>
2023-07-07 22:49:48 +1000

    Fixed project positioning + navToggle

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function navActive() {
+
+// When user clicks menu icon
+function navToggle() {
     document.getElementById("dropdown").classList.toggle("active");
+    console.log("Toggled");
+}
+
+// When user clicks anywhere else
+window.onclick = function(event) {
+    if(!event.target.matches(".bx-menu")) {
+        var dropdown = document.getElementById("dropdown");
+        if(dropdown.classList.contains("active")) {
+            dropdown.classList.remove("active");
+            console.log("Removed");
+        }
+    }
 }
