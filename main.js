d9a5ecdc77eee051dbf1aa8d5617460dc5b0048b
Kirkster101 <83754901+Kirkster101@users.noreply.github.com>
2023-07-08 17:30:23 +1000

    Contact Form Functionality

--- main.js
+++ main.js
@@ -15,3 +15,57 @@ window.onclick = function(event) {
         }
     }
 }
+
+// When the user submits the form
+function formSubmit() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var msg = document.getElementById("msg");
+
+    document.getElementById("submit-response").style.visibility = "hidden";
+
+    if(name.value == "") {
+        name.style.border = "2px solid red";
+    } else {
+        name.style.border = "2px solid white";
+    }
+
+    if(email.value == "") {
+        email.style.border = "2px solid red";
+    } else {
+        email.style.border = "2px solid white";
+    }
+
+    if(msg.value == "") {
+        msg.style.border = "2px solid red";
+    } else {
+        msg.style.border = "2px solid white";
+    }
+
+    if(name.value != "" && email.value != "" && msg.value != "") {
+        sendMail();
+    }
+}
+
+function sendMail(params) {
+    var tempParams = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("msg").value
+    }
+
+    document.getElementById("contact-btn").innerHTML = "<i class='bx bx-loader-circle bx-spin'></i>";
+
+    emailjs
+        .send("service_etgdhzb","template_6ntgkzp",tempParams)
+        .then(function(res) {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("msg").value = "";
+
+            document.getElementById("contact-btn").innerHTML = "Submit";
+            document.getElementById("submit-response").style.visibility = "visible";
+
+            console.log("Success", res.status);
+        });
+}
